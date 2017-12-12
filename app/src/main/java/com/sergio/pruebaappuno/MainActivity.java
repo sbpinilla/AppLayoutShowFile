93c1cf9db2f1f0c8282c64cd47ce298b3282ea1b
sbpinilla <sbpinilla@hotmail.com>
2017-12-11 22:07:23 -0500

    Commit Inicial

---
+++ app/src/main/java/com/sergio/pruebaappuno/MainActivity.java
@@ -0,0 +1,18 @@
+package com.sergio.pruebaappuno;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ArrayAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
